ac5400764d3bb06dace501c2c8222123fd87b3da
Andrea Bicskei <andrea.bcsk@gmail.com>
2022-07-13 11:26:30 +0200

    exercise1_done

---
+++ exercise1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise1</title>
+</head>
+<body>
+ 
+<?php 
+
+$a = 0;
+
+for ($i=0; $i<51; $i++)
+{
+    $a = "Andrea Bicskei";
+}
+
+echo ("My name is: $a");
+
+?>
+
+
+</body>
+</html>
